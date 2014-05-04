fa743a9a65c5b474e2418f11db79078599469dad
Filip Bogyai <fbogyai@redhat.com>
2014-05-05 00:49:03 +0200

    Get handlers of certificate-tracker subsystem into CertificateTrackerDefinition

--- src/main/java/org/jboss/certificate/tracker/extension/CertificateTrackerDefinition.java
+++ src/main/java/org/jboss/certificate/tracker/extension/CertificateTrackerDefinition.java
@@ -1,8 +1,19 @@
 package org.jboss.certificate.tracker.extension;
 
+import java.util.List;
+
+import org.jboss.as.controller.AbstractBoottimeAddStepHandler;
+import org.jboss.as.controller.AbstractRemoveStepHandler;
+import org.jboss.as.controller.OperationContext;
+import org.jboss.as.controller.OperationFailedException;
+import org.jboss.as.controller.ServiceVerificationHandler;
 import org.jboss.as.controller.SimpleResourceDefinition;
+import org.jboss.dmr.ModelNode;
+import org.jboss.msc.service.ServiceController;
 
 /**
+ * Root definition of certificate tracker subsystem
+ * 
  * @author Filip Bogyai
  */
 public class CertificateTrackerDefinition extends SimpleResourceDefinition {
@@ -15,6 +26,43 @@ public class CertificateTrackerDefinition extends SimpleResourceDefinition {
                 CertificateTrackerAdd.INSTANCE,
                 //Every resource that is added, normally needs a remove operation
                 CertificateTrackerRemove.INSTANCE);
+    }
+
+    /**
+     * Handler responsible for adding the certificate tracker subsystem resource
+     * to the model
+     */
+    static class CertificateTrackerAdd extends AbstractBoottimeAddStepHandler {
+
+        static final CertificateTrackerAdd INSTANCE = new CertificateTrackerAdd();
+
+        private CertificateTrackerAdd() {
+        }
+
+        @Override
+        protected void performBoottime(OperationContext context, ModelNode operation, ModelNode model,
+                ServiceVerificationHandler verificationHandler, List<ServiceController<?>> newControllers) throws OperationFailedException {
+
+        }
+
+        @Override
+        protected void populateModel(ModelNode operation, ModelNode model) throws OperationFailedException {
+
+        }
+
+    }
+
+    /**
+     * Handler responsible for removing the certificate tracker subsystem
+     * resource from the model
+     */
+    static class CertificateTrackerRemove extends AbstractRemoveStepHandler {
+
+        static final CertificateTrackerRemove INSTANCE = new CertificateTrackerRemove();
+
+        private CertificateTrackerRemove() {
+        }
+
     }
 
 }
