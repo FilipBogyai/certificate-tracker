9f60beb3d052c7a2e64f6b46b390a938f24de4e1
Filip Bogyai <fbogyai@redhat.com>
2014-05-06 21:32:01 +0200

    Added LGPLv3 license information

--- src/main/java/org/jboss/certificate/tracker/client/dogtag/CertDataInfos.java
+++ src/main/java/org/jboss/certificate/tracker/client/dogtag/CertDataInfos.java
@@ -1,20 +1,21 @@
-//--- BEGIN COPYRIGHT BLOCK ---
-//This program is free software; you can redistribute it and/or modify
-//it under the terms of the GNU General Public License as published by
-//the Free Software Foundation; version 2 of the License.
-//
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
-//
-//You should have received a copy of the GNU General Public License along
-//with this program; if not, write to the Free Software Foundation, Inc.,
-//51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-//
-//(C) 2012 Red Hat, Inc.
-//All rights reserved.
-//--- END COPYRIGHT BLOCK ---
+/** Copyright 2014 Filip Bogyai
+ *
+ * This file is part of certificate-tracker.
+ *
+ * Certificate-tracker is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package org.jboss.certificate.tracker.client.dogtag;
 
 import java.util.Collection;
